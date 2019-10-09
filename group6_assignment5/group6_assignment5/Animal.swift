dc8180141a40e75d53fc410a18eed9223c44d026
Denis Bowen <42016383+DBowen33@users.noreply.github.com>
2019-10-09 17:36:52 -0500

    changed animal file

--- group6_assignment5/group6_assignment5/Animal.swift
+++ group6_assignment5/group6_assignment5/Animal.swift
@@ -10,16 +10,44 @@ import Foundation
 
 class Animal {
     
-    private var name:String = ""
-    private var scientificName:String = ""
-    private var animalClass:String = ""
-    private var size:Float = 0.0
+    // MARK: Properties
     
-    init(name:String, scientificName:String, animalClass:String, size:Float) {
+    var name: String
+    var scientificName: String
+    var animalClass: String
+    var size: Float
+    
+    // MARK: Initialization
+    
+    init?(name: String, scientificName: String, animalClass: String, size: Float) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The scientific name must not be empty
+        guard !scientificName.isEmpty else {
+            return nil
+        }
+        
+        // The animal class must not be empty
+        guard !animalClass.isEmpty else {
+            return nil
+        }
+        
+        // Size must greater than 0
+        guard size > 0 else {
+            return nil
+        }
+      
+        
+        // Initialize stored properties
         self.name = name
         self.scientificName = scientificName
         self.animalClass = animalClass
         self.size = size
+    
     }
 }
 
